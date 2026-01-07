42557b89fb72f82c8728fd062171abd7e5b57e9b
Kumkum Solanki <kumkumsolanki65@gmail.com>
2026-01-07 21:13:13 +0530

    signup and login api

--- disney_backend/main.go
+++ disney_backend/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"disney/database"
+	"disney/handlers"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	database.InitDB()
+
+	// Create Gin router
+	router := gin.Default()
+
+	// Auth routes
+	auth := router.Group("/api/auth")
+	{
+		auth.POST("/signup", handlers.Signup)
+		auth.POST("/login", handlers.Login)
+	}
+
+	port := ":8080"
+	fmt.Println("Server running on port", port)
+	router.Run(port)
 }
